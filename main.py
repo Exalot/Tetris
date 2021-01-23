79b4a08cbd0f6c7fcec260ef3e27d1c308e3c5d4
cubecode22 <piroozeftekhar@gmail.com>
2021-01-23 18:19:17 -0500

    Replaced use of tuples with Vector2

--- main.py
+++ main.py
@@ -7,21 +7,22 @@ import random
 import constants
 import copy
 
+from vector2 import Vector2
+
 
 class TetrisAlgorithm(object):
     """
     Runs the Tetris game and demonstrates the algorithm
     """
 
-    def __init__(self):
-        self.tetrisDimensions = (10, 24)
+    def __init__(self) -> None:
+        self.tetrisDimensions = Vector2(10, 24)
         self.blockSize = 40
-        self.screenSize = (screenSizeX, screenSizeY) = (
-            self.tetrisDimensions[constants.x] * self.blockSize, self.tetrisDimensions[constants.y] * self.blockSize)
-        self.screen = pygame.display.set_mode(self.screenSize)
+        self.blockOffset = Vector2(0, 0)
+        self.screenSize = Vector2(self.tetrisDimensions.x * self.blockSize, self.tetrisDimensions.y * self.blockSize)
+        self.screen = pygame.display.set_mode(self.screenSize.to_tuple())
         self.clock = pygame.time.Clock()
-        self.matrix = [[0 for i in range(self.tetrisDimensions[constants.x])] for j in range(self.tetrisDimensions[
-                                                                                                 constants.y])]
+        self.matrix = [[0 for i in range(self.tetrisDimensions.x)] for j in range(self.tetrisDimensions.y)]
         self.pieceList = constants.get_pieces()
         self.rotation = 0
         self.movingPieceTemplate = []
@@ -29,18 +30,17 @@ class TetrisAlgorithm(object):
         self.game_over = False
         self.score = 0
 
-    def __print_matrix(self):
+    def __print_matrix(self) -> None:
         for row in self.matrix:
             print(row)
 
-    def __get_moving_piece_coordinate(self) -> list[tuple[int, int]]:
+    def __get_moving_piece_coordinate(self) -> list[Vector2]:
         return copy.deepcopy(self.movingPieceCoordinates)
 
-    def __get_moving_piece_future_coordinate(self, direction: tuple[int, int]) -> list[tuple[int, int]]:
-        return [(piece_coordinate[constants.x] + direction[constants.x], piece_coordinate[constants.y] + direction[
-            constants.y]) for piece_coordinate in self.movingPieceCoordinates]
-    
-    def end_game(self):
+    def __get_moving_piece_future_coordinate(self, direction: Vector2) -> list[Vector2]:
+        return [piece_coordinate + direction for piece_coordinate in self.movingPieceCoordinates]
+
+    def end_game(self) -> None:
         """
         ends game
         """
@@ -52,7 +52,7 @@ class TetrisAlgorithm(object):
         endText2 = constants.end_font2.render(f"You achieved a score of: {self.score}", True, constants.white)
         """
 
-    def process_piece_spawn(self, piece_list: list[list[list[int]]]):
+    def process_piece_spawn(self, piece_list: list[list[list[int]]]) -> None:
         """
         finds the co-ordinates of the new moving pieces and add their colors to the matrix
         :param piece_list: The piece selected from the list of pieces
@@ -61,50 +61,55 @@ class TetrisAlgorithm(object):
             for x in range(len(piece_list[self.rotation][y])):
                 if piece_list[self.rotation][y][x]:
                     if not self.matrix[y][x]:
-                        self.movingPieceCoordinates.append((x, y))
+                        self.movingPieceCoordinates.append(Vector2(x, y))
                         self.matrix[y][x] = piece_list[self.rotation][y][x]
                     else:
                         self.game_over = True
 
-    def spawn_piece(self):
+    def spawn_piece(self) -> None:
         """
         spawns a piece at the top of the matrix
         """
         self.movingPieceCoordinates.clear()
         self.rotation = 0
+        self.blockOffset = Vector2(0, 0)
         if len(self.pieceList) == 0:
             self.pieceList = constants.get_pieces()
         self.movingPieceTemplate = random.choice(self.pieceList)
         self.pieceList.remove(self.movingPieceTemplate)
         self.process_piece_spawn(self.movingPieceTemplate)
 
-    def can_move_direction(self, direction: tuple[int, int]) -> bool:
+    def can_move_direction(self, direction: Vector2) -> bool:
         """
         Checks if the moving piece can move down any further
         :param direction: the direction of the movement
         :return: boolean representing if the selected piece can move down
         """
-        for x, y in self.__get_moving_piece_future_coordinate(direction):
-            if (x, y >= 0) and not (x < self.tetrisDimensions[constants.x] and y < self.tetrisDimensions[constants.y]):
+        for futurePiece in self.__get_moving_piece_future_coordinate(direction):
+            if (futurePiece.x, futurePiece.y >= 0) and not (futurePiece.x < self.tetrisDimensions.x and futurePiece.y <
+                                                            self.tetrisDimensions.y):
                 return False
-            elif self.matrix[y][x] and tuple([x, y]) not in self.movingPieceCoordinates:
+            elif self.matrix[futurePiece.y][futurePiece.x] and True not in [Vector2(futurePiece.x, futurePiece.y) ==
+                                                                            movingBlock for movingBlock in
+                                                                            self.movingPieceCoordinates]:
                 return False
         return True
 
-    def move_direction(self, direction: tuple[int, int]):
+    def move_direction(self, direction: Vector2) -> None:
         """
         moves the moving piece in specified direction
         :param direction: the direction of the movement
         """
-        color = self.matrix[self.movingPieceCoordinates[0][constants.y]][self.movingPieceCoordinates[0][constants.x]]
-        newCoordinates = self.__get_moving_piece_future_coordinate(direction)
-        for x, y in self.movingPieceCoordinates:
-            self.matrix[y][x] = 0
-        for x, y in newCoordinates:
-            self.matrix[y][x] = color
-        self.movingPieceCoordinates = newCoordinates
+        color = self.matrix[self.movingPieceCoordinates[0].y][self.movingPieceCoordinates[0].x]
+        new_coordinates = self.__get_moving_piece_future_coordinate(direction)
+        for movingPiece in self.movingPieceCoordinates:
+            self.matrix[movingPiece.y][movingPiece.x] = 0
+        for newPiece in new_coordinates:
+            self.matrix[newPiece.y][newPiece.x] = color
+        self.movingPieceCoordinates = new_coordinates
+        self.blockOffset += direction
 
-    def hard_drop(self):
+    def hard_drop(self) -> None:
         """
         Hard drops the current piece
         """
@@ -112,22 +117,21 @@ class TetrisAlgorithm(object):
             self.move_direction(constants.DOWN)
         self.spawn_piece()
 
-    def rotate(self):
+    def rotate(self, clockwise: bool) -> None:
         """
         rotates the selected piece
         """
         self.rotation = (self.rotation + 1) % len(self.movingPieceTemplate)
 
-    def find_block_coordinates(self, x: int, y: int) -> tuple[int, int]:
+    def find_block_coordinates(self, position: Vector2) -> Vector2:
         """
         helper function to find the x and y co-ordinates of block in matrix
-        :param x: x index of block
-        :param y: y index of block
+        :param position: A vector2 representing the x, y location of the block
         :return: tuple representing x, y co-ordinates of block
         """
-        return self.blockSize * x, self.blockSize * y
+        return position * self.blockSize
 
-    def draw_matrix(self):
+    def draw_matrix(self) -> None:
         """
         Draws the matrix on the screen
         """
@@ -135,12 +139,12 @@ class TetrisAlgorithm(object):
         for y in range(len(self.matrix)):
             for x in range(len(self.matrix[y])):
                 if self.matrix[y][x] != 0:
-                    co_ordinates = self.find_block_coordinates(x, y)
-                    rect = pygame.Rect(co_ordinates[constants.x], co_ordinates[constants.y], self.blockSize, self.
+                    co_ordinates = self.find_block_coordinates(Vector2(x, y))
+                    rect = pygame.Rect(co_ordinates.x, co_ordinates.y, self.blockSize, self.
                                        blockSize)
                     pygame.draw.rect(self.screen, constants.colorDict[self.matrix[y][x]], rect)
 
-    def main(self):
+    def main(self) -> None:
         """
         Runs the game
         """
@@ -171,17 +175,23 @@ class TetrisAlgorithm(object):
                         elif event.key == pygame.K_LEFT:
                             if self.can_move_direction(constants.LEFT):
                                 self.move_direction(constants.LEFT)
+                        elif event.key == pygame.K_x:
+                            self.rotate(True)
+                            pass  # TODO: Implement clockwise rotation
+                        elif event.key == pygame.K_z:
+                            self.rotate(False)
+                            pass  # TODO: Implement counter clockwise rotation
                 self.draw_matrix()
-                scoreText = constants.score_font.render(f"Score: {self.score}", True, constants.white)
-                self.screen.blit(scoreText, (self.screenSize[constants.x] - constants.score_font_shift[constants.x],
-                                             constants.score_font_shift[constants.y]))
+                score_text = constants.score_font.render(f"Score: {self.score}", True, constants.white)
+                self.screen.blit(score_text, (self.screenSize.x - constants.score_font_shift.x,
+                                              constants.score_font_shift.y))
             else:
                 print("Game Over")
                 for event in pygame.event.get():
                     if event.key == pygame.QUIT:
                         pygame.quit()
                         running = False
-                    elif event.key == pygame.K_ESCAPE:
+                    elif event.key == pygame.K_ESCAPE and constants.debugMode:
                         pygame.quit()
                         running = False
             pygame.display.update()
--- vector2.py
+++ vector2.py
@@ -2,12 +2,27 @@ import math
 
 
 class Vector2:
-    def __init__(self, x: float, y: float):
+    def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
 
     def __str__(self):
         return f"({self.x}, {self.y})"
+
+    def __bool__(self):
+        return not self.x and self.y
+
+    def __int__(self):
+        return int(abs(self))
+
+    def __float__(self):
+        return float(abs(self))
+
+    def __complex__(self):
+        return complex(self.x, self.y)
+
+    def __abs__(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
 
     def __add__(self, other):
         return Vector2(self.x + other.x, self.y + other.y)
@@ -26,10 +41,10 @@ class Vector2:
     def __div__(self, other):
         # vector compression: (x1, y1) / a = (x1/a, y1/a)
         if isinstance(other, int) or isinstance(other, float):
-            return Vector2(self.x / other, self.y / other)
+            return Vector2(int(self.x / int(other)), int(self.y / other))
         # vector compression: (x1, y1) / (x2, y2) = (x1/x2, y1/y2)
         elif isinstance(other, Vector2):
-            return Vector2(self.x / other.x, self.y / other.y)
+            return Vector2(self.x // other.x, self.y // other.y)
 
     def __eq__(self, other):
         # magnitude comparison: sqrt(x1**2, y1**2) == a
@@ -39,11 +54,8 @@ class Vector2:
         elif isinstance(other, Vector2):
             return self.x == other.x and self.y == other.y
 
-    def __complex__(self):
-        return complex(self.x, self.y)
-
-    def magnitude(self):
-        return math.sqrt(self.x ** 2 + self.y ** 2)
-
     def normalized(self):
         return Vector2(self.x / self.magnitude(), self.y / self.magnitude())
+
+    def to_tuple(self) -> tuple[int, int]:
+        return self.x, self.y
